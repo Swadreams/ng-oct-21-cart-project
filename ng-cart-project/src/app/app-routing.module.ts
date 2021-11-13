bb1d8b5809c0e06f419e6cf26e88bfcfec1ddd2a
Swadreams <swadreams@gmail.com>
2021-11-13 08:51:52 +0530

    Updated appication with Routes

--- ng-cart-project/src/app/app-routing.module.ts
+++ ng-cart-project/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductComponent } from './components/product/product.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'products', component: ProductComponent },
+  { path: 'products-detail', component: ProductDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
