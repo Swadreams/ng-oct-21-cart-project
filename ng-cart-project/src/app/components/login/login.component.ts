c429456c21b8389659c2474d4db3cae5d260392f
Swadreams <swadreams@gmail.com>
2021-11-20 11:12:22 +0530

    Integrated login api

--- ng-cart-project/src/app/components/login/login.component.ts
+++ ng-cart-project/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css', '../../shared/form.style.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  isLoading = false;
+  error = '';
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
   onLogin(form: any) {
     console.log(form.value);
+    this.isLoading = true;
+    this.authService
+      .login(form.value)
+      .subscribe(
+        () => {
+          this.router.navigate(['/products']);
+        },
+        (error) => {
+          this.error = error.message;
+        }
+      )
+      .add(() => {
+        this.isLoading = false;
+      });
   }
 }
