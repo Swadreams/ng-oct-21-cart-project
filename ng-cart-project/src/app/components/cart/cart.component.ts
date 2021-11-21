58c619d971ed127a6d43e2f5bb01ae8a40552946
Swadreams <swadreams@gmail.com>
2021-11-21 09:35:28 +0530

    Created cart page and integrated get cart items and delete cart item api

--- ng-cart-project/src/app/components/cart/cart.component.ts
+++ ng-cart-project/src/app/components/cart/cart.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdcutService } from 'src/app/shared/product.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  cartItems: any[] = [];
+  showLoading = false;
 
-  constructor() { }
+  constructor(private productService: ProdcutService) {}
 
   ngOnInit(): void {
+    this.showLoading = true;
+    this.getCartItems();
   }
 
+  getCartItems() {
+    this.productService
+      .getCart()
+      .subscribe(
+        (response: any) => {
+          this.productService.addAllItemsToCart(response);
+          this.cartItems = response;
+        },
+        (error) => {
+          console.log('Cart items error :', error);
+        }
+      )
+      .add(() => {
+        this.showLoading = false;
+      });
+  }
+
+  deleteItem(id: string) {
+    this.productService
+      .deleteCartItem(id)
+      .subscribe(
+        () => {
+          this.getCartItems();
+          alert('Item deleted successfully');
+        },
+        () => {
+          alert('Can not delete item at the moment.');
+        }
+      )
+      .add(() => {
+        this.showLoading = false;
+      });
+  }
 }
